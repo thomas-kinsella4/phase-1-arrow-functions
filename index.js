6a95e9710ccfbdc8ccfc359fdc429f52bf5ecbaf
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-08-04 20:43:32 -0400

    commit tree e5dfc4eb2dda1689f520641e94a57c328585eb2f

---
+++ index.js
@@ -0,0 +1,23 @@
+// function divide() {
+//     return 2000 / 100;
+// }
+
+// const square = num => num*num;
+
+// const add = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+
+
+
+
+const divide = function() {
+    return 2000 / 100;
+}
+
+const square = param => param * param;
+
+const add = (num1, num2) => num1 + num2;
+
+
